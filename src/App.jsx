2f25be58f62510a0dc51fba488e9b51a17e456ac
Artemiy Artamonov <array.clean@gmail.com>
2023-07-13 05:01:23 +0300

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import './App.css';
-import { WagmiConfig, createConfig } from "wagmi";
-import { polygonMumbai } from 'wagmi/chains';
-import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
+import "./App.css";
+import { createConfig, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import {
+    ConnectKitButton,
+    ConnectKitProvider,
+    getDefaultConfig,
+} from "connectkit";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { Fragment } from "react";
 
 const config = createConfig(
     getDefaultConfig({
-        alchemyId: 'K7c6nsX9dY6D4OhdtkKc2f05yEcFdtqU',
-        walletConnectProjectId: '1d63d7e43fd1d5ea177bdb4a8939ade4',
+        alchemyId: "K7c6nsX9dY6D4OhdtkKc2f05yEcFdtqU",
+        walletConnectProjectId: "1d63d7e43fd1d5ea177bdb4a8939ade4",
 
         chains: [polygonMumbai],
 
         // Required
-        appName: 'ARCANUM',
+        appName: "ARCANUM",
 
         // Optional
-        appDescription: 'Decentralized asset management protocol that allows to create complicated asset management portfolio and manage them in a DeFi manner.',
-        appUrl: 'https://arcanum.to',
-        appIcon: 'https://arcanum.to/logo.png'
-    })
+        appDescription:
+            "Decentralized asset management protocol that allows to create complicated asset management portfolio and manage them in a DeFi manner.",
+        appUrl: "https://arcanum.to",
+        appIcon: "https://arcanum.to/logo.png",
+    }),
 );
-
 
 function App() {
     return (
-        <WagmiConfig config={config}>
-            <ConnectKitProvider>
-                    /* Your App */
-                <ConnectKitButton />
-            </ConnectKitProvider>
-        </WagmiConfig>
+        <Router>
+            <WagmiConfig config={config}>
+                <ConnectKitProvider>
+                    <main>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/swap">Swap</Link>
+                                </li>
+                                <li>
+                                    <Link to="/arbi">$ARBI</Link>
+                                </li>
+                                <li>
+                                    <ConnectKitButton />
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <Routes>
+                            <Route path="/" exact element={<Home />} />
+                            <Route path="/swap" element={<Home />} />
+                            <Route path="/arbi" element={<Home />} />
+                            <Route path="*" exact element={<NotFound />} />
+                        </Routes>
+                    </main>
+                </ConnectKitProvider>
+            </WagmiConfig>
+        </Router>
     );
 }
+
+const Home = () => (
+    <Fragment>
+        <h1>Home</h1>
+    </Fragment>
+);
+const NotFound = () => <h1>Not found</h1>;
 
 export default App;
