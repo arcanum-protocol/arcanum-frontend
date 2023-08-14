1bca4010dd4740743d9f28d15fa4e3afefce68a7
Artemiy Artamonov <array.clean@gmail.com>
2023-08-14 17:01:03 +0300

    change bini to bali

--- src/main.tsx
+++ src/main.tsx
@@ -35,7 +35,7 @@ const router = createBrowserRouter([
                 element: <Cpt />,
             },
             {
-                path: "/bini",
+                path: "/bali",
                 element: <Bini />,
             },
         ]
