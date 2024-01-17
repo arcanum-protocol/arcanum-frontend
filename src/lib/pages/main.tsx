a57e647c00b0254bcd0d6db95a58b0b87b73cf72
Talgat The Random <eureq631@gmail.com>
2024-01-17 12:02:03 +0300

    Update SVG icons and logo size

--- src/lib/pages/main.tsx
+++ src/lib/pages/main.tsx
@@ -168,9 +168,9 @@ export const Head = observer(() => {
 
     return (
         <div className='flex w-full rounded p-1 justify-between items-center bg-[#0c0a09] border border-[#292524]'>
-            <div className="flex flex-row items-center justify-between gap-2 pl-6 pr-8 py-2 xl:py-0 w-full">
+            <div className="flex flex-row items-center justify-between gap-2 pl-4 pr-8 py-2 xl:py-0 w-full">
                 <div className="flex flex-row text-left gap-2 items-center">
-                    <img src={logo} alt="Logo" className='w-8 h-8' />
+                    <img src={`/multipools/${multipoolId}_eclipse.svg`} alt="Logo" className='w-12 h-12' />
                     <div>
                         <p className='text-[#fff] p-0 text-2xl'>{multipoolId.toLocaleUpperCase() || ""}</p>
                         <div className='flex flex-row gap-2 items-center text-white opacity-70 cursor-pointer' onClick={copyToClipboard}>
