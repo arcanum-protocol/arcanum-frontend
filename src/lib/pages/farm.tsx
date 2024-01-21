fcc6abdf7493695ba402fb0a42bf0e2384647245
Talgat The Random <eureq631@gmail.com>
2024-01-22 01:15:06 +0300

    Update farms.yaml and farm.tsx

--- src/lib/pages/farm.tsx
+++ src/lib/pages/farm.tsx
@@ -3,11 +3,33 @@ import { useQuery } from "@tanstack/react-query"
 
 
 function Farms() {
-    const { data: farms, isLoading, isError } = useQuery(['farms'], () => fetchFarms(), { refetchOnWindowFocus: false });
+    const { data, isLoading, isError } = useQuery(['farms'], () => fetchFarms(), { refetchOnWindowFocus: false });
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
+    const { farms } = data;
+    console.log(farms);
+
+    if (isError) {
+        return <div>Error...</div>
+    }
 
     return (
-        <div>
-            <h1>Farms</h1>
+        <div className="grid grid-cols-6 gap-1">
+            {
+                farms?.map((farm: any) => {
+                    return (
+                        <div className="border rounded bg-[#0c0a09]">
+                            <div className="flex items-start">
+                                <img src={farm.logo} className="w-10 h-10" />
+                                <div className="text-gray-300 text-2xl">{farm.name}</div>
+                            </div>
+                            <div className="text-gray-300 text-sm">{farm.token}</div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
