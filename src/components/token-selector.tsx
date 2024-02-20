0959f45b7345b2096e01c6729c6f715a3cbeff96
Talgat The Random <eureq631@gmail.com>
2024-02-20 16:51:18 +0300

    Update token selector UI

--- src/components/token-selector.tsx
+++ src/components/token-selector.tsx
@@ -166,8 +166,11 @@ const TokenSelector = observer(({ action }: TokenSelectorProps) => {
 
     return (
         <>
-            <div className="py-1">
-                <Input placeholder="Search for a token" value={search} onChange={(e) => setSearch(e.target.value)} />
+            <div className="inline-flex w-full gap-2 py-1">
+                <div className="flex w-[11%] items-center justify-center border rounded p-[0.2rem] cursor-pointer" onClick={() => setSelectedTabWrapper("back")}>
+                    <svg width='50' viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                </div>
+                <Input className="w-11/12" placeholder="Search for a token" value={search} onChange={(e) => setSearch(e.target.value)} />
             </div>
             <ScrollArea className="h-[478px] w-full py-1">
                 {
