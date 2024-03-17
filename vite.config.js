fb4ac58da67f4eb098b1c00d65eef5955ca3bf3e
Talgat The Random <eureq631@gmail.com>
2024-03-17 22:28:00 +0300

    Update vite.config.js with alias and define ENVIRONMENT

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import ViteYaml from '@modyfi/vite-plugin-yaml';
 
-export default defineConfig({
-  plugins: [ViteYaml(), react(), myPlugin({
-    ENVIRONMENT: process.env.ENVIRONMENT,
-  })],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(() => {
+  return {
+    plugins: [ViteYaml(), react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    define: {
+      ENVIRONMENT: process.env.ENVIRONMENT || "localhost",
     },
   }
 })
