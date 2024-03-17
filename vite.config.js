bdc59edd621c85d335f61f4a996999dc19cc6369
Talgat The Random <eureq631@gmail.com>
2024-03-17 22:22:40 +0300

    Add myPlugin to Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react';
 import ViteYaml from '@modyfi/vite-plugin-yaml';
 
 export default defineConfig({
-  plugins: [ViteYaml(), react()],
+  plugins: [ViteYaml(), react(), myPlugin({
+    ENVIRONMENT: process.env.ENVIRONMENT,
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  define: {
-    __APP_ENV__: process.env.ENVIRONMENT,
-  },
+  }
 })
