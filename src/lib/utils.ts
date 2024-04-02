2109f0855eebebf339d81d4b4219418f601ff9d9
Talgat The Random <eureq631@gmail.com>
2024-04-02 17:07:16 +0300

    Fix import statements and update dollar value formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,7 +15,7 @@ export function fromX32(value: bigint): bigint {
 
 export function fromX96(value: bigint | undefined, decimals = 18): BigNumber {
   if (value === undefined) return new BigNumber(0);
-  
+
   const _decimals = 2n ** 96n;
 
   const fromUniswapType = new BigNumber(value.toString()).div(_decimals.toString());
@@ -38,25 +38,42 @@ export function tohumanReadableQuantity(number: BigNumber, decimals = 18) {
   const subsrint = ["₀", "₁", "₂", "₃", "₄", "₅", "₆", "₇", "₈", "₉"];
   const _decimals = new BigNumber(10).pow(decimals);
   if (number.isEqualTo(0)) {
-      return "0";
+    return "0";
   }
   if (number.dividedBy(_decimals).isLessThan(0.001)) {
-      const _number = number.dividedBy(_decimals).toFixed();
-      const numberWithout_zerodotzero = _number.substring(3, _number.length);
+    const _number = number.dividedBy(_decimals).toFixed();
+    const numberWithout_zerodotzero = _number.substring(3, _number.length);
 
-      // regex to remove trailing zeros
-      const numberWithoutTrailingZeros = numberWithout_zerodotzero.replace(/^0+(?=\d)/, '');
-      const trailingZerosCount = numberWithout_zerodotzero.length - numberWithoutTrailingZeros.length;
-      // replase the zeros with the subscript
-      const numberWithSubscript = trailingZerosCount.toString().split("").map((char) => subsrint[parseInt(char)]).join("");
+    // regex to remove trailing zeros
+    const numberWithoutTrailingZeros = numberWithout_zerodotzero.replace(/^0+(?=\d)/, '');
+    const trailingZerosCount = numberWithout_zerodotzero.length - numberWithoutTrailingZeros.length;
+    // replase the zeros with the subscript
+    const numberWithSubscript = trailingZerosCount.toString().split("").map((char) => subsrint[parseInt(char)]).join("");
 
-      return `0.0${numberWithSubscript}${numberWithoutTrailingZeros}`;
+    return `0.0${numberWithSubscript}${numberWithoutTrailingZeros}`;
   } else {
-      const _decimals = new BigNumber(10).pow(decimals);
-      const _number = new BigNumber(number.toString());
+    const _decimals = new BigNumber(10).pow(decimals);
+    const _number = new BigNumber(number.toString());
 
-      const value = _number.dividedBy(_decimals);
-      return value.toFixed(3);
+    const value = _number.dividedBy(_decimals);
+    return value.toFixed(3);
+  }
+}
+
+export function toHumanReadableDollarValue(value: BigNumber) {
+  // k - thousand, m - million, b - billion, t - trillion, q - quadrillion
+  if (value.isLessThan(1000)) {
+    return value.toFixed(2);
+  } else if (value.isLessThan(1000000)) {
+    return `${(value.dividedBy(1000)).toFixed(2)}k`;
+  } else if (value.isLessThan(1000000000)) {
+    return `${(value.dividedBy(1000000)).toFixed(2)}m`;
+  } else if (value.isLessThan(1000000000000)) {
+    return `${(value.dividedBy(1000000000)).toFixed(2)}b`;
+  } else if (value.isLessThan(1000000000000000)) {
+    return `${(value.dividedBy(1000000000000)).toFixed(2)}t`;
+  } else {
+    return `${(value.dividedBy(1000000000000000)).toFixed(2)}q`;
   }
 }
 
